af88ac0d0f76ac0764cbc1b42532c2fe1611a681
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-05-04 10:11:12 +0300

    add

---
+++ src/basic/3.ts
@@ -0,0 +1,10 @@
+let myMixedType: string | number;
+
+myMixedType = "string";
+myMixedType = 10;
+
+type EnableOrdisable = "enable" | "disable";
+
+let myAnswer: EnableOrdisable;
+myAnswer = "enable";
+myAnswer = "disable";
